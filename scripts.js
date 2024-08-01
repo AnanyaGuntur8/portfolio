07b418f556fd312732174cfbd4c0c33b130ab9a1
AnanyaGuntur8 <149723548+AnanyaGuntur8@users.noreply.github.com>
2024-08-01 11:20:17 -0400

    Commit to Main

--- scripts.js
+++ scripts.js
@@ -17,6 +17,19 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+function openButton(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("button-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 function flipCard(card) {
     card.classList.toggle('flipped');
 }
